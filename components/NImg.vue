ce30dad3445f9e9e45d76b38696873f89332f40e
ltt <ltt7716@163.com>
2025-07-30 10:54:21 +0800

    img

---
+++ components/NImg.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <p>使用相对路径</p>
+    <img src="../assets/img.png" alt="Layers1" />
+    <p>使用别名路径，会指向根项目</p>
+    <img src="@/assets/img.png" alt="Layers1" />
+  </div>
+</template>
