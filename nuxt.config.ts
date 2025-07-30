4f74f9347b3b405231f2c93dcc3b7b3f5e9f5805
ltt <ltt7716@163.com>
2025-07-30 10:32:40 +0800

    nuxt-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   devtools: { enabled: true },
   $meta: {
     name: 'example11',
   },
+  css: [
+    join(currentDir, './assets/main.css')
+  ],
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport-1', content: 'width=device-width, initial-scale=1' }
+      ],
+    }
+  }
 })
