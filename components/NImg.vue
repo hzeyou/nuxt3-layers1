017c26a52944f323073bd5c999f0e25f2225e784
ltt <ltt7716@163.com>
2025-07-30 10:58:18 +0800

    NImg

--- components/NImg.vue
+++ components/NImg.vue
@@ -4,9 +4,6 @@
 
 <template>
   <div>
-    <p>使用相对路径</p>
-    <img src="../assets/img.png" alt="Layers1" />
-    <p>使用别名路径，会指向根项目</p>
     <img src="@/assets/img.png" alt="Layers1" />
   </div>
 </template>
